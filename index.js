5994eeef4d7e3babec171ff2b6f69a43c478bba8
Alexandru Dumitru Maroz <alexandru666@outlook.es>
2024-03-17 23:33:54 +0100

    Adding Github Oauth

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
+const base64Tools = require("./exports/base64ArrayBuffer.js")
+
 const express = require('express')
 const logger = require('morgan')
 const fs = require('fs');
 const https = require('https')
-const base64Tools = require("./exports/base64ArrayBuffer.js")
+const config = require('config')
 
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const JwtStrategy  = require('passport-jwt').Strategy
+const GitHubStrategy = require('passport-github2').Strategy
 const jwt = require('jsonwebtoken')
 const jwtSecret = require('crypto').randomBytes(16) // 16*8=256 random bits
 
 const cookieParser = require('cookie-parser')
 
-// KDF
-const scryptPbkdf = require('scrypt-pbkdf')
-const derivedKeyLength = 32
-const salt = scryptPbkdf.salt(32)
-const scryptFastParams = {
-    N: 262144,
-    r: 8,
-    p: 1
-}
-const scryptSlowParams = {
-    N: 2097152,
-    r: 8,
-    p: 1
-}
 ///////////////////
 // SCRYPT CONFIG //
 ///////////////////
-const scryptParams = scryptFastParams
+const scryptPbkdf = require('scrypt-pbkdf')
+const derivedKeyLength = config.get('scrypt.derivedKeyLength')
+const salt = scryptPbkdf.salt(derivedKeyLength)
+const scryptParams = config.get('scrypt.FastParams')
+
+//////////////////
+// GITHUB OAUTH //
+//////////////////
+const GITHUB_CLIENT_ID = config.get('github.clientID')
+const GITHUB_CLIENT_SECRET = config.get('github.clientSecret')
+const GITHUB_CALLBACK_URL = config.get('github.callbackURL')
 
 const app = express()
-const port = 3000
+const host = config.get('server.host')
+const port = config.get('server.port')
+
 
 app.use(logger('dev'))
 
@@ -98,81 +99,106 @@ The local strategy requires a `verify` function which receives the credentials
 that the username and password are correct and then invoke `done` with a user
 object, which will be set at `req.user` in route handlers after authentication.
 */
-passport.use('login-username-password', new LocalStrategy(
-    {
-        usernameField: 'username',  // it MUST match the name of the input field for the username in the login HTML formulary
-        passwordField: 'password',  // it MUST match the name of the input field for the password in the login HTML formulary
-        session: false // we will store a JWT in the cookie with all the required session data. Our server does not need to keep a session, it's going to be stateless
-    },
-    async function (username, password, done) {
-        user = getUser(username)
-        if (user === null) {
-            return done(null, false)
-        }
+passport.use('login-username-password',
+    new LocalStrategy(
+        {
+            usernameField: 'username',  // it MUST match the name of the input field for the username in the login HTML formulary
+            passwordField: 'password',  // it MUST match the name of the input field for the password in the login HTML formulary
+            session: false // we will store a JWT in the cookie with all the required session data. Our server does not need to keep a session, it's going to be stateless
+        },
+        async function (username, password, done) {
+            user = getUser(username)
+            if (user === null) {
+                return done(null, false)
+            }
 
-        const hash = await scryptPbkdf.scrypt(password, salt, derivedKeyLength, scryptParams)
-        const hashString = base64Tools.base64ArrayBuffer(hash)
-        if (user.password === hashString) {
+            const hash = await scryptPbkdf.scrypt(password, salt, derivedKeyLength, scryptParams)
+            const hashString = base64Tools.base64ArrayBuffer(hash)
+            if (user.password === hashString) {
+                const user = {
+                    username: username,
+                    description: 'the "only" user that deserves to get to this server'
+                }
+                return done(null, user) // the first argument for done is the error, if any. In our case there is no error, and so we pass null. The object user will be added by the passport middleware to req.user and thus will be available there for the next middleware and/or the route handler
+            }
+
+            return done(null, false)  // in passport returning false as the user object means that the authentication process failed.
+        }
+    )
+)
+
+passport.use('signup-username-password',
+    new LocalStrategy(
+        {
+            usernameField: 'username',  // it MUST match the name of the input field for the username in the login HTML formulary
+            passwordField: 'password',  // it MUST match the name of the input field for the password in the login HTML formulary
+            session: false // we will store a JWT in the cookie with all the required session data. Our server does not need to keep a session, it's going to be stateless
+        },
+        async function (username, password, done) {
+            if(existsUser(username)) {
+                return done(null, false)
+            }
+
+            const hash = await scryptPbkdf.scrypt(password, salt, derivedKeyLength, scryptParams)
+            const hashString = base64Tools.base64ArrayBuffer(hash)
+            addUser(username, hashString, 'fast')
             const user = {
                 username: username,
                 description: 'the "only" user that deserves to get to this server'
             }
-            return done(null, user) // the first argument for done is the error, if any. In our case there is no error, and so we pass null. The object user will be added by the passport middleware to req.user and thus will be available there for the next middleware and/or the route handler
-        }
-
-        return done(null, false)  // in passport returning false as the user object means that the authentication process failed.
-    }
-))
-
-passport.use('signup-username-password', new LocalStrategy(
-    {
-        usernameField: 'username',  // it MUST match the name of the input field for the username in the login HTML formulary
-        passwordField: 'password',  // it MUST match the name of the input field for the password in the login HTML formulary
-        session: false // we will store a JWT in the cookie with all the required session data. Our server does not need to keep a session, it's going to be stateless
-    },
-    async function (username, password, done) {
-        if(existsUser(username)) {
-            return done(null, false)
-        }
-
-        const hash = await scryptPbkdf.scrypt(password, salt, derivedKeyLength, scryptParams)
-        const hashString = base64Tools.base64ArrayBuffer(hash)
-        addUser(username, hashString, 'fast')
-        const user = {
-            username: username,
-            description: 'the "only" user that deserves to get to this server'
-        }
-        return done(null, user)
-    }
-))
-
-passport.use('jwtCookie', new JwtStrategy(
-    {
-        jwtFromRequest: (req) => {
-            if (req && req.cookies) return req.cookies.jwt
-            return null
-        },
-        secretOrKey: jwtSecret
-    },
-    function (jwtPayload, done) {
-        if (jwtPayload.sub) {
-            const user = {
-                username: jwtPayload.sub,
-                description: 'one of the users that deserve to get to this server',
-                role: jwtPayload.role ?? 'user'
-            }
             return done(null, user)
         }
-        return done(null, false)
-    }
-))
+    )
+)
+
+passport.use('github-oauth',
+    new GitHubStrategy(
+        {
+            clientID: GITHUB_CLIENT_ID,
+            clientSecret: GITHUB_CLIENT_SECRET,
+            callbackURL: GITHUB_CALLBACK_URL
+        },
+        function(accessToken, refreshToken, profile, done) {
+            if (!profile.username) {
+                return done(null, false)
+            }
+            if (!existsUser(profile.username)) {
+                addUser(profile.username, accessToken)
+            }
+            return done(null, profile)
+        }
+    )
+)
+
+passport.use('jwtCookie',
+    new JwtStrategy(
+        {
+            jwtFromRequest: (req) => {
+                if (req && req.cookies) return req.cookies.jwt
+                return null
+            },
+            secretOrKey: jwtSecret
+        },
+        function (jwtPayload, done) {
+            if (jwtPayload.sub) {
+                const user = {
+                    username: jwtPayload.sub,
+                    description: 'one of the users that deserve to get to this server',
+                    role: jwtPayload.role ?? 'user'
+                }
+                return done(null, user)
+            }
+            return done(null, false)
+        }
+    )
+)
 
 function generateToken(user) {
     // This is what ends up in our JWT
     const jwtClaims = {
         sub: user.username,
-        iss: 'localhost:3000',
-        aud: 'localhost:3000',
+        iss: `${host}:${port}`,
+        aud: `${host}:${port}`,
         exp: Math.floor(Date.now() / 1000) + 604800, // 1 week (7×24×60×60=604800s) from now
         role: 'user' // just to show a private JWT field
     }
@@ -196,27 +222,46 @@ app.get('/',
     }
 )
 
-app.get('/login', (req, res) => {
-    res.sendFile('routes/login.html', { root: __dirname })
-})
-
-app.post('/login',
-  passport.authenticate('login-username-password', { session: false, failureRedirect: '/login' }), // we indicate that this endpoint must pass through our 'username-password' passport strategy, which we defined before
-  (req, res) => {
-    const token = generateToken(req.user)
-
-    res.cookie('jwt', token, { httpOnly: true, secure: true }) // Write the token to a cookie with name 'jwt' and enable the flags httpOnly and secure.
-    res.redirect('/')
-
-    // And let us log a link to the jwt.io debugger for easy checking/verifying:
-    console.log(`Token sent. Debug at https://jwt.io/?value=${token}`)
-    console.log(`Token secret (for verifying the signature): ${jwtSecret.toString('base64')}`)
-  }
+app.get('/login',
+    (req, res) => {
+        res.sendFile('routes/login.html', { root: __dirname })
+    }
 )
 
-app.get('/signup', (req, res) => {
-    res.sendFile('routes/signup.html', { root: __dirname })
-})
+app.post('/login',
+    passport.authenticate('login-username-password', { session: false, failureRedirect: '/login' }), // we indicate that this endpoint must pass through our 'username-password' passport strategy, which we defined before
+    (req, res) => {
+        const token = generateToken(req.user)
+
+        res.cookie('jwt', token, { httpOnly: true, secure: true }) // Write the token to a cookie with name 'jwt' and enable the flags httpOnly and secure.
+        res.redirect('/')
+
+        // And let us log a link to the jwt.io debugger for easy checking/verifying:
+        console.log(`Token sent. Debug at https://jwt.io/?value=${token}`)
+        console.log(`Token secret (for verifying the signature): ${jwtSecret.toString('base64')}`)
+    }
+)
+
+app.get("/auth/github",
+    passport.authenticate("github-oauth", { scope: ["repo:status"] }), /// Note the scope here
+    (req, res) => { }
+)
+
+app.get("/auth/github/callback",
+    passport.authenticate("github-oauth", { session: false, failureRedirect: "/login" }),
+    (req, res) => {
+        const token = generateToken(req.user)
+
+        res.cookie('jwt', token, { httpOnly: true, secure: true }) // Write the token to a cookie with name 'jwt' and enable the flags httpOnly and secure.
+        res.redirect('/')
+    }
+)
+
+app.get('/signup',
+    (req, res) => {
+        res.sendFile('routes/signup.html', { root: __dirname })
+    }
+)
 
 app.post('/signup',
   passport.authenticate('signup-username-password', { session: false, failureRedirect: '/signup' }), // we indicate that this endpoint must pass through our 'username-password' passport strategy, which we defined before
@@ -232,10 +277,12 @@ app.post('/signup',
   }
 )
 
-app.get('/logout', (req, res) => {
-    res.clearCookie('jwt')
-    res.redirect('/')
-})
+app.get('/logout',
+    (req, res) => {
+        res.clearCookie('jwt')
+        res.redirect('/')
+    }
+)
 
 ///////////////////
 // ERROR HANDLER //
@@ -250,5 +297,5 @@ https.createServer({
     key: fs.readFileSync('./certs/server.key'),
     cert: fs.readFileSync('./certs/server.cert')
 }, app).listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at https://${host}:${port}`)
 })
